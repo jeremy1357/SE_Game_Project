d79ba80c40e0727096c7db32f19bf81f991a9bd1
Jeremy Bowler <jeremyb124@outlook.com>
2020-09-10 18:57:11 -0700

    Added the SDL library to the project with the correct linking in the project properties. The library is located in deps

--- SE_Game_Project/main.cpp
+++ SE_Game_Project/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// To allow these includes to work you must specify the include and lib
+// pathway. Already set so dont change
+#include <SDL/SDL.h> 
+
 
 int main() {
 	std::cout << "Start of game!\n";
