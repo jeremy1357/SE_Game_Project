37316430d7eb1d83609747e074d590e17a82adc9
Corey Deeves <coreydeeves2@gmail.com>
2020-10-13 17:02:30 -0400

    Updated FPS_Timer

--- SE_Game_Project/FPS_Timer.h
+++ SE_Game_Project/FPS_Timer.h
@@ -1,18 +1,16 @@
 #pragma once
 #include <chrono>
-#include <iostream>
 #include <thread>
 
 using namespace std::chrono;
-steady_clock::time_point first_tp;
-unsigned int m_fps;
-unsigned long fps_counter = 0;
 
 class FPS_Timer
 {
-public:	
-	duration<double> uptime();
-	double fps();
-	void run_fps();
-};
+public:
+	duration<double> Start_fps();
+	duration<double> End_fps();
 
+	steady_clock::time_point first_tp;
+	double m_fps;
+	double m_fps_timer;
+};
