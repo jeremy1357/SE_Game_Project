a0402a22d4d01d1b3de89b23f3e3df8d41aa81c9
Corey Deeves <cd370438@wne.edu>
2020-11-14 19:27:12 -0500

    ParticleManager Changes

--- SE_Game_Project/ParticleManager.cpp
+++ SE_Game_Project/ParticleManager.cpp
@@ -16,19 +16,16 @@ void ParticleManager::update_particle(float health, bool isActive)
 {
 
 	for (int i = 0; i < MAX_PARTICLE_COUNT; i++) {
-		if (m_particles[i].lifeTime > 0.0) {
+		if (m_particles[i].health > 0.0) {
 			m_particles[i].isActive = true;
 			m_particles[i].position += m_particles[i].direction * m_particles[i].speed;
-			m_particles[i].lifeTime -= 1.0f;
+			m_particles[i].health-= 1.0f;
 
 			bool check = m_collisionManager->is_square_on_restricted_tile(m_particles[i].position, m_particles[i].size);
 			if (check == true)
 			{
-				m_particles[i].lifeTime = 0.0f;
-				for (i = i; i < m_active_particles; i++)
-				{
-					m_particles[i] = m_particles[i + 1];
-				}
+				m_particles[i].isActive = false;
+				m_particles[i].health = 0.0f;
 				m_active_particles--;
 			}					
 		}
@@ -38,14 +35,20 @@ void ParticleManager::update_particle(float health, bool isActive)
 	}
 }
 
-void ParticleManager::update_AddParticle(glm::vec3 pos, glm::vec3 dir)
+void ParticleManager::update_AddParticle(glm::vec2 pos, glm::vec2 dir, ColorRGBA32 color)
 {
 	// Find an inactive particle within the array
-	for (int i = 0; i < MAX_PARTICLE_COUNT; i++) {
-		if (m_particles[i].isActive == false) {
+	for (int i = 0; i < MAX_PARTICLE_COUNT; i++) 
+	{
+		if (m_particles[i].isActive == false) 
+		{
 			m_particles[i].isActive = true;
-			m_particles[i].position = dir;
-			m_particles[i].lifeTime -= 1.0f; // This value might change
+			m_particles[i].position = pos;
+			m_particles[i].direction = dir;
+			m_particles[i].size = glm::vec2(5.0, 5.0);
+			m_particles[i].speed = glm::vec2(0.5, 0.5);
+			m_particles[i].health = 100.0f;
+			m_particles[i].m_color = color;
 			m_active_particles++;
 		}
 	}
